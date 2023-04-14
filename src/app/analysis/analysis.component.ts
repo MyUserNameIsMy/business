9e8158d5fabd99a88e8374ced0e6a77007602e07
myusernameismy <ilyas123.822@gmail.com>
2023-04-14 07:13:57 +0600

    finished table

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-analysis',
@@ -8,7 +9,9 @@ import {HttpClient} from "@angular/common/http";
 })
 export class AnalysisComponent {
   selectedFile!: File;
-  analysisResult!: string;
+  resultReceived: boolean = false;
+  public analysisResult!: {'npv': any, 'irr': any, 'pi': any, 'pbpd': any, 'verdict_npv': any, 'verdict_irr': any,
+    'verdict_pi': any, 'recommendations': any, 'debt_ratio': any, 'equity_ratio': any}
 
   constructor(private http: HttpClient) {
   }
@@ -22,8 +25,9 @@ export class AnalysisComponent {
 
     this.http.post('http://localhost:5000/analyze', formData).subscribe(
       (response: any) => {
-        console.log(response)
-        this.analysisResult = response.result;
+        console.log(response.data)
+        this.analysisResult = JSON.parse(JSON.stringify(response.data));
+        this.resultReceived = true;
       },
       (error) => {
         console.log(error); // handle the error
